84cfd7fcf38741d787c89235b79973e595dbfd2a
ebrahim5599 <eng.ebrahim59@gmail.com>
2024-07-01 12:50:29 +0300

    Final Edit

--- app/src/main/java/com/example/roomcodelab/WordDao.kt
+++ app/src/main/java/com/example/roomcodelab/WordDao.kt
@@ -1,11 +1,9 @@
-package com.example.roomcodelab.repo
+package com.example.roomcodelab
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.roomcodelab.pojo.Word
 import kotlinx.coroutines.flow.Flow
 
 @Dao
